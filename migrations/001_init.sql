6747ed5f24623fed712eb3fb235a918507b7fca3
Nelson Guijarro <nrguijarro@puce.edu.ec>
2025-07-10 23:28:00 -0500

    Corregido 2

--- migrations/001_init.sql
+++ migrations/001_init.sql
@@ -1,2 +1,17 @@
-CREATE TABLE IF NOT EXISTS usuarios ( ... );
-CREATE TABLE IF NOT EXISTS horarios ( ... );
+CREATE TABLE IF NOT EXISTS usuarios (
+  id SERIAL PRIMARY KEY,
+  nombres TEXT NOT NULL,
+  cedula VARCHAR(10) UNIQUE NOT NULL,
+  correo TEXT UNIQUE NOT NULL CHECK (correo LIKE '%@puce.edu.ec'),
+  telefono TEXT NOT NULL,
+  usuario TEXT UNIQUE NOT NULL,
+  contrasena TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS horarios (
+  id SERIAL PRIMARY KEY,
+  dia VARCHAR(10) NOT NULL,
+  hora_entrada TIME NOT NULL,
+  hora_salida TIME NOT NULL,
+  usuario_id INTEGER NOT NULL,
+  FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+);
