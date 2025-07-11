de5a3ef61213a61d7078d6d20e8af2ea6c63b3ca
Nelson Guijarro <nrguijarro@puce.edu.ec>
2025-07-10 23:14:25 -0500

    Corregido

--- migrations/migrate.js
+++ migrations/migrate.js
@@ -1,5 +1,7 @@
 import fs from 'fs';
 import db from '../backend/db.js';
-await db.query(fs.readFileSync('migrations/001_init.sql').toString());
+await db.query(
+  fs.readFileSync('migrations/001_init.sql', 'utf8')
+);
 console.log('✔ Migración aplicada');
 process.exit();
